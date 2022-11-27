e31ba0003400f3a9877623e8ac8bfb12f045872a
tan-jerome <2100337A@student.tp.edu.sg>
2022-11-28 00:21:37 +0800

    Update Collectables.cs

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -6,28 +6,28 @@ public class Collectables : MonoBehaviour
 {
     public Collider2D colButton;  //collider at the end of a level
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Collectables")  //tags with the name collectables
-        {
-            Destroy(collision.gameObject);          //destroy collectable
-            //colButton.GetComponent<Collider>().isTrigger = true;  //collider on tirgger is enabled
-        }
-    }
-
-
-
-
+    ArrayList collectArray = new ArrayList(); //Arraylist storing collectables
 
     // Start is called before the first frame update
     void Start()
     {
-        
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Collectables")  //tags with the name collectables
+        {
+            Destroy(collision.gameObject);          //destroy collectable
+            //colButton.GetComponent<Collider>().isTrigger = true;  //collider on tirgger is enabled
+
+            collectArray.Add(collision.gameObject); //Adds the seed_bag into the Arraylist
+        }
     }
 }
