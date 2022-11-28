a16d338e5447551b8752adbfaddce40b470d7f16
stevelim0217 <stevelim0217@gmail.com>
2022-11-28 23:05:17 +0800

    Added Platforms

--- Assets/Scripts/GnomeMovement.cs
+++ Assets/Scripts/GnomeMovement.cs
@@ -97,7 +97,7 @@ public class GnomeMovement : MonoBehaviour
             }
             if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
             {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 1.5f);
             }
         }
     }
