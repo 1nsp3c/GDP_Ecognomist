f908378f3e85d3d262e904e064a2eae1711f1110
tan-jerome <2100337A@student.tp.edu.sg>
2022-11-29 00:58:20 +0800

    Added Seed Count UI

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
     //public Collider2D colButton;  //collider at the end of a level
+    public GameObject seedText;
+    private TextMeshProUGUI textMeshSeed;
 
     ArrayList collectArray = new ArrayList(); //Arraylist storing collectables
 
     // Start is called before the first frame update
     void Start()
     {
-   
+        textMeshSeed = seedText.GetComponent<TextMeshProUGUI>(); //Grabs the TextMeshProGui of the seed Texts
     }
+
 
     // Update is called once per frame
     void Update()
     {
-
+        textMeshSeed.text = "Count : " + collectArray.Count;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
