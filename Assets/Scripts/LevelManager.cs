c9ef1bf9a184bb731904e16637b8b2108a47564c
tan-jerome <2100337A@student.tp.edu.sg>
2022-12-18 16:43:02 +0800

    Level Select (yall can change the look if wan)

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void mainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+    public void level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
