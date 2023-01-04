bd4e99b9afa240f87be3ac5e5cc27a9087bb7e04
stevelim0217 <stevelim0217@gmail.com>
2023-01-04 23:03:46 +0800

    Done enemy

--- Assets/Scripts/Level2/EnemySpawner.cs
+++ Assets/Scripts/Level2/EnemySpawner.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    private float spawnInterval = 1f;
+    private float spawnInterval = 1.5f;
     public GameObject spawner;
-    
+    private int spawnCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(spawnEnemy(spawnInterval, enemyPrefab));
         
+        StartCoroutine(spawnEnemy(spawnInterval, enemyPrefab));
     }
 
     private IEnumerator spawnEnemy(float interval, GameObject enemy) 
-    { 
-        yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, spawner.transform.position, Quaternion.identity);
-        StartCoroutine(spawnEnemy(interval, enemy));
+    {
+        while (spawnCount < 2) 
+        {
+            yield return new WaitForSeconds(interval);
+            GameObject newEnemy = Instantiate(enemy, spawner.transform.position, Quaternion.identity);
+            spawnCount += 1;
+        }
+        
     }
     
 }
