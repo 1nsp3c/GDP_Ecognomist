f28278624db0d9645fc31a5deb9e251b1e75379b
1nsp3c <82948413+1nsp3c@users.noreply.github.com>
2023-01-10 15:40:28 +0800

    dfwefewfcwefcwecfvewdcvfeqsvedqveqaveqv

--- Assets/Scripts/Level1/Tree.cs
+++ Assets/Scripts/Level1/Tree.cs
@@ -11,11 +11,13 @@ public class Tree : MonoBehaviour
     public Animator animator;
     public BoxCollider2D boxCollider2D;
     Level2TempBar level2TempBar;
+    Level2Gnome level2Gnome;
 
     public List<GameObject> treeList = new List<GameObject>();
     private void Start()
     {
         level2TempBar = FindObjectOfType<Level2TempBar>();
+        level2Gnome = FindObjectOfType<Level2Gnome>();
         animator = GetComponent<Animator>();
         boxCollider2D = GetComponent<BoxCollider2D>();
         Physics2D.IgnoreLayerCollision(9, 10); //Ignore collision with player
@@ -26,9 +28,11 @@ public class Tree : MonoBehaviour
     }
     public void ResetTempBar()
     {
-        if (timesHit >= 3)
+        if (timesHit == 3)
         {
             extinguish = true;
+            level2Gnome.AddEnergy1(6);
+            boxCollider2D.enabled = false;
             level2TempBar.ResetSlider();
             animator.SetBool("extinguished", extinguish);
         }
