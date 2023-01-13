ee381faf18203e89679c093fbfa813dcf9af4463
1nsp3c <82948413+1nsp3c@users.noreply.github.com>
2023-01-13 15:37:18 +0800

    SignBoards hint when player over them

--- Assets/Scripts/Level2/SignBoards.cs
+++ Assets/Scripts/Level2/SignBoards.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SignBoards : MonoBehaviour
 {
-    public TMPro signboardText;
+    public TextMeshProUGUI signboardText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        signboardText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            signboardText.gameObject.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            signboardText.gameObject.SetActive(false);
+        }
     }
 }
