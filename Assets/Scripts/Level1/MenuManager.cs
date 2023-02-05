03996c931ff63937d65c2987249477e4c88f86d7
1nsp3c <82948413+1nsp3c@users.noreply.github.com>
2023-02-06 00:18:04 +0800

    g

--- Assets/Scripts/Level1/MenuManager.cs
+++ Assets/Scripts/Level1/MenuManager.cs
@@ -9,14 +9,17 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     public void Restart()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     public void levelSelect()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("Level Select");
     }
     public void Cutscene()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene("CutScene");
     }
 }
