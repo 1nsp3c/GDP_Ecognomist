9dd104269fa78d2780ca8e8b2a95e54a7a290e44
tan-jerome <2100337A@student.tp.edu.sg>
2023-02-06 20:45:36 +0800

    Help page ui and codez

--- Assets/Scripts/Level1/MainMenuManager.cs
+++ Assets/Scripts/Level1/MainMenuManager.cs
@@ -5,7 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject helpPage;
+    public GameObject general;
+    public GameObject level1;
+    public GameObject level2;
+
+    public int currentIndex = 0;
+
+    private bool helpActive = false;
+
+
     // Start is called before the first frame update
+
+    public void Update()
+    {
+        if (helpActive)
+        {
+            if (currentIndex == 0)
+            {
+                general.SetActive(true);
+                level1.SetActive(false);
+                level2.SetActive(false);
+            }
+            if (currentIndex == 1)
+            {
+                general.SetActive(false);
+                level1.SetActive(true);
+                level2.SetActive(false);
+            }
+            if (currentIndex == 2)
+            {
+                general.SetActive(false);
+                level1.SetActive(false);
+                level2.SetActive(true);
+            }
+        }
+        else
+        {
+            general.SetActive(false);
+            level1.SetActive(false);
+            level2.SetActive(false);
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("Level Select");
@@ -13,5 +54,26 @@ public class MainMenuManager : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void HelpPage()
+    {
+        helpPage.SetActive(true);
+        helpActive = true;
+    }
+
+    public void Close()
+    {
+        helpPage.SetActive(false);
+        helpActive = false;
+    }
+
+    public void Next()
+    {
+        currentIndex += 1;
+    }
+    public void Previous()
+    {
+        currentIndex -= 1;
     }
 }
